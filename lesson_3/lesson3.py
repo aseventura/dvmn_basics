4f0de262d29ab3ba8924d45c9c516c5af67a1517
Yaroslav <y.suvorov@yadro.com>
2019-10-15 13:41:53 +0300

    Добавление третьего урока

---
+++ lesson_3/lesson3.py
@@ -0,0 +1,40 @@
+import smtplib
+import os
+
+template_message = """Привет, %friend_name%! %my_name% приглашает тебя на сайт %website%!
+
+%website% — это новая версия онлайн-курса по программированию. 
+Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя. 
+
+Как будет проходить ваше обучение на %website%? 
+
+→ Попрактикуешься на реальных кейсах. 
+Задачи от тимлидов со стажем от 10 лет в программировании.
+→ Будешь учиться без стресса и бессонных ночей. 
+Задачи не «сгорят» и не уйдут к другому. После окончания курса у тебя будет 2 месяца, чтобы догнать программу. 
+→ Подготовишь крепкое резюме.
+Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
+
+Регистрируйся → %website%  
+На модули, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл."""
+
+personal_message = template_message.replace('%friend_name%', 'Ярослав')\
+    .replace('%my_name%', 'Евгений').replace('%website%', 'dvmn.org')
+
+from_addr = 'yaroslav.dvmn1@yandex.ru'
+to_addr = 'scrat.93@mail.ru'
+
+mail = """From: {from_addr} 
+To: {to_addr}
+Subject: Ревью!
+Content-Type: text/plain; charset="UTF-8";
+
+{msg}""".format(from_addr = from_addr, to_addr = to_addr, msg = personal_message)\
+    .encode('UTF-8')
+
+server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
+login = os.getenv('login')
+password = os.getenv('password')
+server.login(login, password)
+server.sendmail(from_addr, to_addr, mail)
+server.quit()
